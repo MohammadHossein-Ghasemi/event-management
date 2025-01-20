d8128d321ad00e8cd6afaf4ddd4f8baea257f27a
Mohammad Hossein <s.m.h.gh.1382@gmail.com>
2025-01-20 15:37:21 +0330

    set up project

---
+++ eventManagement/src/test/java/co/muhu/eventManagement/EventManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package co.muhu.eventManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
