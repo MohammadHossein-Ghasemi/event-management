ad550ecaa46570d547d5409bcf5835cc780d8b56
Mohammad Hossein <s.m.h.gh.1382@gmail.com>
2025-01-25 22:28:47 +0330

    Fix ConcurrentModificationException with my custom query.

--- eventManagement/src/main/java/co/muhu/eventManagement/repository/EventRepository.java
+++ eventManagement/src/main/java/co/muhu/eventManagement/repository/EventRepository.java
@@ -2,8 +2,23 @@ package co.muhu.eventManagement.repository;
 
 import co.muhu.eventManagement.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
+
+import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event,Long> {
     boolean existsById(@NonNull Long id);
+
+    @Override
+    @Query("SELECT e FROM event e " +
+            "LEFT JOIN FETCH e.participantSet " +
+            "LEFT JOIN FETCH e.memberset " +
+            "LEFT JOIN FETCH e.ticketSet " +
+            "LEFT JOIN FETCH e.feedBackSet " +
+            "LEFT JOIN FETCH e.organizer " +
+            "LEFT JOIN FETCH e.venue " +
+            "WHERE e.id = :id")
+    Optional<Event> findById(@NonNull @Param("id") Long id);
 }
